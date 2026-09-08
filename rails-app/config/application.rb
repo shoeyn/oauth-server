feca9a792c992d2d5dbdaa3baee7fa81c654a0d2
Nathan <nathan@cyberfreakz.com>
2026-09-08 18:20:01 +0100

    Dependency updates

--- rails-app/config/application.rb
+++ rails-app/config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RailsLoginApp
   class Application < Rails::Application
-    config.load_defaults 7.1
+    config.load_defaults 8.0
     config.api_only = false
     config.secret_key_base = ENV.fetch("SECRET_KEY_BASE", "a" * 64)
   end
